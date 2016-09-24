072d8b62ddd368937bcd5d896a3ee421cc1e5f82
邓文峰 <dwfn3398@corp.netease.com>
2016-09-24 10:57:22 +0800

    modify port

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 
 const express = require('express')
 
+const port = process.env.PORT || 3000
 const app = express()
 
 app.get('/', function(req, res) {
     res.send('Hello world')
 })
-app.listen(80, function(){
+app.listen(port, function(){
     console.log('app listen on 80')
 })
